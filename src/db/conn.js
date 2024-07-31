a1490785681e5cbce820d51a4a4027e9669129f6
Ayush5ah <sahayush89@gmail.com>
2024-07-31 17:40:10 +0530

    slight changes

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const password = process.env.PASSWORD
 
-mongoose.connect(`mongodb+srv://sahayush89:<password>@cluster1.9upb8xd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`)
+mongoose.connect(`mongodb+srv://sahayush89:${password}@cluster1.9upb8xd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`)
 .then( () => {
     console.log('Connection to database successful')
 })
